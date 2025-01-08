01be5368c7494fbc87f97dd25e39e870c00e2124
lillebjorns <142815773+lillebjorns@users.noreply.github.com>
2025-01-08 14:32:42 +0100

    Add files via upload

---
+++ demo-2-github-actions/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>GitHub Actions Demo</h1>
+      <p>
+        Om du ser denna sida betyder det att GitHub Actions deployade sidan
+        framgångsrikt!
+      </p>
+    </div>
+  );
+}
+
+export default App;
